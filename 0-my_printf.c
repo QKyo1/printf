db2b5d97ab4159a54ea29c443de1d7a6e03ee61f
qkyo1 <engawab8@gmail.com>
2023-11-14 06:00:30 -0800

    print f

---
+++ 0-my_printf.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+* print_buff - function to print the buffer
+* @buff: atgumet of buffer
+* @buff_ind: Index
+*/
+
+void print_buff(char buff[], int *buff_ind)
+{
+if (*buff_ind > 0)
+write(1, &buffer[0], *buff_ind);
+*buff_ind = 0;
+}
+
+/**
+* _printf - my print f function
+* @format: argument of the format
+* Return:
+*/
+
+int _printf(const char *format, ...)
+{
+int flg, wid, pre, siz, buff_ind = 0;
+int i, pr = 0, printed_cha = 0;
+va_list list;
+char buff[BUFF_SIZE];
+if (format == NULL)
+return (-1);
+va_start(list, format);
+for (i = 0; format && format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+buff[buff_ind++] = format[i];
+if (buff_ind == BUFF_SIZE)
+print_buff(buff, &buff_ind);
+printed_cha++;
+}
+else
+{
+print_buff(buffer, &buff_ind);
+flg = ha_flags(format, &i);
+wid = ha_width(format, &i, list);
+pre = ha_precision(format, &i, list);
+siz = ha_size(format, &i);
+++i;
+pr = ha_printf(format, &i, list, buff, flg, wid, pre, siz);
+if (pr == -1)
+return (-1);
+printed_cha += pr;
+}
+}
+print_buff(buff, &buff_ind);
+va_end(list);
+return (printed_cha);
+}
--- main.h
+++ main.h
@@ -1,22 +1,79 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include <unistd.h>
-#define BUFFER_SIZE 1024
+
+#define S_LONG 2
+#define S_SHORT 1
+
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+#define BUFF_SIZE 1024
 
 /**
-* cr - counter and restaring of counter
-* @coun: start of the counter
-* @ret: restarting of the counter
-* @Describtion: tracks buffer and the restaring of the count3r
+* struct fm - structure
+* @fm: Format
+* @fn: Thee function
 */
+struct fm
+{
+char fm;
+int (*fm)(va_list, char[], int, int, int, int);
+};
 
-typedef struct cr {
-int coun;
-int ret;
-} cn;
+
+/**
+* typedef struct fm match - Structure
+* @fm: format of it
+* @match: The function associated.
+*/
+typedef struct fm match;
 
 int _printf(const char *format, ...);
+int ha_printf(const char *fm, int *i, va_list list, char buff[], int flg, int wid, int pre, int siz);
+
+
+int _putchar(char c);
+int printchr(va_list types, char buff[], int flg, int wid, int pre, int siz);
+int printstr(va_list types, char buff[], int flg, int wid, int pre, int siz);
+int print37(va_list types, char buff[], int flg, int wid, int pre, int siz);
+int printine(va_list types, char buff[], int flg, int wid, int pre, int siz);
+int printbin(va_list types, char buff[], int flg, int wid, int pre, int siz);
+int printfund(va_list types, char buff[], int flg, int wid, int pre, int siz);
+int printoct(va_list types, char buff[], int flg, int wid, int pre, int siz);
+int printhex(va_list types, char buff[], int flg, int wid, int pre, int siz);
+int printHEX(va_list types, char buff[], int flg, int wid, int pre, int siz);
+int printhexa(va_list types, char map_to[], char buff[], int flg, char flg_ch, int wid, int pre, int siz);
+int printex_str(va_list types, char buff[], int flg, int wid, int pre, int siz);
+int printpoi(va_list types, char buff[], int flg, int wid, int pre, int siz);
+int printfrev(va_list types, char buff[], int flg, int wid, int pre, int siz);
+int printro13(va_list types, char buff[], int flg, int wid, int pre, int siz);
+
+
+int ha_flg(const char *format, int *i);
+int ha_wid(const char *format, int *i, va_list list);
+int ha_pre(const char *format, int *i, va_list list);
+int ha_siz(const char *format, int *i);
+
+
+int handle_write_char(char c, char buff[], int flg, int wid, int pre, int siz);
+int write_number(int is_positive, int end, char buff[], int flg, int wid, int pre, int siz);
+int write_num(int end, char buff[], int flg, int wid, int pre, int leen, char pad, char ex_c);
+int write_pointer(char buff[], int end, int leen, int wid, int flg, char pad, char ex_c, int pad_start);
+
+int write_unsgnd(int is_neg, int end, char buff[], int flg, int wid, int pre, int siz);
+
+
+long int con_si_nu(int size, long int nu);
+long int con_un_si(unsigned long int numb, int siz);
+
+int is_ex(char);
+int append_code(char, char[], int);
+int isDigit(char);
 
 #endif
