912c1c4f173e082bf6d89b3f935e197eb14454b2
qkyo1 <engawab8@gmail.com>
2023-11-14 01:49:43 -0800

    delets

--- main.h
+++ main.h
@@ -1,29 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
-#define BUFF_SIZE 1024
-
+#define BUFFER_SIZE 1024
 
 /**
-* cr - counter + return count
-* @counter: counter
-* @ret_count: return counter
-* @Describtion: tracks the buffer index and the return
+* cr - counter and restaring of counter
+* @coun: start of the counter
+* @ret: restarting of the counter
+* @Describtion: tracks buffer and the restaring of the count3r
 */
 
 typedef struct cr {
-int counter;
-int ret_count;
-} stu;
+int coun;
+int ret;
+} cn;
 
 int _printf(const char *format, ...);
-stu putchar(char c, char *buff, stu counter);
-stu printD(long a, stu counter, char *buf);
-stu printI(long a, stu counter, char *buf);
-stu printB(unsigned int a, stu counter, char *buf);
-stu printS(char *a, stu counter, char *buf);
 
 #endif
